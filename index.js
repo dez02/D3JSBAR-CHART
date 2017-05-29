6efabd8ccb0faea1864a7c75be1d992df67c4ae3
Désirée <deutodesiree@gmail.com>
2017-05-29 22:08:10 +0200

    created svg element

---
+++ index.js
@@ -0,0 +1,22 @@
+var dataset = [ 5, 10, 13, 19, 21, 25, 22, 18, 15, 13,
+                11, 12, 15, 20, 18, 17, 16, 18, 23, 25 ];
+
+//Width and height
+var svgWidth = 500;
+var svgheight = 100;
+
+//Create SVG element
+var svg = d3.select("body")
+			.append("svg")
+			.attr("width", svgWidth)
+            .attr("height", svgheight);
+
+
+    svg.selectAll("rect")
+	   .data(dataset)
+	   .enter()
+	   .append("rect")
+	   .attr("x", 0)
+	   .attr("y", 0)
+	   .attr("width", 20)
+	   .attr("height", 100);
